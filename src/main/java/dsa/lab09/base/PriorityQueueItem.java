65a0adc2d32402446b1da921578466f4ddf5397f
Bertie Wheen <bertie@wheen.online>
2026-03-21 15:55:26 +0000

    lab 9

---
+++ src/main/java/dsa/lab09/base/PriorityQueueItem.java
@@ -0,0 +1,100 @@
+package dsa.lab09.base;
+
+import dsa.lib.DSAObject;
+import dsa.lib.utils.ToStringUtils;
+
+import java.util.Objects;
+
+/**
+ * An item in a priority queue.
+ *
+ * @param <Priority> the priority type
+ * @param <Item>     the item type
+ * @see PriorityQueue
+ */
+public class PriorityQueueItem<Priority extends Comparable<Priority>, Item>
+  extends DSAObject
+  implements Comparable<PriorityQueueItem<Priority, Item>>
+{
+
+  /** The item's priority. */
+  private Priority priority;
+
+
+  /** The item. */
+  private Item item;
+
+
+  /**
+   * Construct a priority queue item.
+   */
+  public PriorityQueueItem(Priority priority, Item item)
+  {
+    this.priority = priority;
+    this.item = item;
+  }
+
+
+  /**
+   * Get the priority.
+   *
+   * @return the priority
+   */
+  public Priority priority()
+  {
+    return this.priority;
+  }
+
+
+  /**
+   * Get the item.
+   *
+   * @return the item
+   */
+  public Item item()
+  {
+    return this.item;
+  }
+
+
+  @Override
+  public int compareTo(PriorityQueueItem<Priority, Item> that)
+  {
+    return this.priority.compareTo(that.priority);
+  }
+
+
+  //<editor-fold defaultstate="collapsed" desc="equals()+hashCode()+toString()">
+
+
+  @Override
+  public boolean equals(Object that)
+  {
+    if (that == null || this.getClass() != that.getClass())
+    {
+      return false;
+    }
+    PriorityQueueItem<?, ?> that_ = (PriorityQueueItem<?, ?>) that;
+    return Objects.equals(this.priority, that_.priority) &&
+      Objects.equals(this.item, that_.item);
+  }
+
+
+  @Override
+  public int hashCode()
+  {
+    return Objects.hash(this.priority, this.item);
+  }
+
+
+  @Override
+  public String toString(String indent)
+  {
+    return "Priority " + ToStringUtils.toString(this.priority, indent) + ":"
+      + " " + ToStringUtils.toString(this.item, indent);
+  }
+
+
+  //</editor-fold>
+
+}
