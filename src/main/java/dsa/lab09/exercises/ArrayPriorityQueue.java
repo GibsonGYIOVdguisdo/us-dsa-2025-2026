b35c504cf5684cb336c24a57d8991090b33d4690
Gibson <114831858+GibsonGYIOVdguisdo@users.noreply.github.com>
2026-04-15 12:58:07 +0100

    Implemented removeMax method

--- src/main/java/dsa/lab09/exercises/ArrayPriorityQueue.java
+++ src/main/java/dsa/lab09/exercises/ArrayPriorityQueue.java
@@ -124,8 +124,22 @@ public class ArrayPriorityQueue<Priority extends Comparable<Priority>, Item>
       return null;
     }
 
-    // TODO: Implement ArrayPriorityQueue.removeMax()
-    return null;
+
+    PriorityQueueItem<Priority, Item> max = this.items.get(0);
+    int index = 0;
+
+    for (int i = 0; i < this.items.size(); i++){
+      PriorityQueueItem<Priority, Item> current = this.items.get(i);
+
+      if (current.compareTo(max) > 0){
+        max = current;
+        index = i;
+      }
+    }
+
+    this.items.remove(index);
+
+    return max;
   }
 
 
